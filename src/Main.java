d23e63c52b8026b3b014faad5aa2ab593b12417f
Barabulos <vidreanrobert@yahoo.com>
2025-01-15 13:22:55 +0200

    1 c tsv

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
 
         WesterosApp.displayNameByInitial(dateiName);
 
-
+        WesterosApp.displaySortedHouseStark(dateiName);
     }
 }
--- src/WesterosApp.java
+++ src/WesterosApp.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class WesterosApp {
@@ -52,5 +55,37 @@ public class WesterosApp {
             System.out.println("Fehler beim Lesen der Datei: " + e.getMessage());
         }
     }
+    public static void displaySortedHouseStark(String dateiName) {
+        List<String> HouseStark = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(dateiName))) {
+            String zeile;
+
+            while ((zeile = br.readLine()) != null) {
+                String[] felder = zeile.split("\t");
+
+                if (felder.length > 2) {
+                    String name = felder[1].trim();
+                    String haus = felder[2].trim();
+
+                    if (haus.equals("Stark")) {
+                        HouseStark.add(name);
+                    }
+                }
+            }
+
+            Collections.sort(HouseStark);
+
+            System.out.println("Alle Haus Stark mitglieder:");
+            for (String name : HouseStark) {
+                System.out.println(name);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Die Datei " + dateiName + " wurde nicht gefunden.");
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei: " + e.getMessage());
+        }
+    }
+
 
 }
